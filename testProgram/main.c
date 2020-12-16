478ae0b8fe67a7f1a5c7e62679faa29fd389ba45
Riften <943275366@qq.com>
2020-12-16 14:16:59 +0800

    add sleep

--- testProgram/main.c
+++ testProgram/main.c
@@ -1,9 +1,11 @@
 #include "CGO/libMesaTracer.h"
 #include "stdio.h"
+#include <unistd.h>
 
 int main() {
     //cgoAddString("test");
     cgoTestEnum(CGO_START);
     cgoTestEnum(CGO_END);
+    sleep(3);
     return 0;
 }

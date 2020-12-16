2696f0baead02bd7b4edf99412cabb6a6286d3dd
Riften <943275366@qq.com>
2020-12-16 13:20:32 +0800

    update

--- testProgram/main.c
+++ testProgram/main.c
@@ -1,8 +1,9 @@
 #include "CGO/libMesaTracer.h"
+#include "stdio.h"
 
 int main() {
-    cgoAddString("test");
-    cgoTestEnum(CGO_START);
-    cgoTestEnum(CGO_END);
+    //cgoAddString("test");
+    //cgoTestEnum(CGO_START);
+    //cgoTestEnum(CGO_END);
     return 0;
 }
